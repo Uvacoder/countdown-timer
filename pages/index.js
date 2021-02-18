4ee19ae43f22c97b57dffc7f3db8848a14e5ddf2
Sam Wight <samuelwight@gmail.com>
2021-02-18 12:17:28 -0600

    Initial version.

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,123 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useMemo } from "react";
+import "tailwindcss/tailwind.css";
+import fs from "fs/promises";
+import path from "path";
+import {
+  parse,
+  add,
+  differenceInMonths,
+  differenceInDays,
+  differenceInHours,
+  differenceInMinutes,
+  differenceInSeconds,
+  format,
+} from "date-fns";
+import { zonedTimeToUtc } from "date-fns-tz";
+import React, { useState, useEffect, useRef } from "react";
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
 }
+
+const dateToText = (date) => {
+  const arr = [];
+  const currDate = new Date();
+  const months = differenceInMonths(new Date(), currDate);
+  const monthDate = add(date, {
+    months,
+  });
+  if (months !== 0) arr.push(`${months} months`);
+
+  const days = differenceInDays(currDate, monthDate);
+  const dayDate = add(monthDate, {
+    days,
+  });
+  if (days !== 0) arr.push(`${days} days`);
+
+  const hours = differenceInHours(currDate, dayDate);
+  const hourDate = add(dayDate, {
+    hours,
+  });
+  if (hours !== 0) arr.push(`${hours} hours`);
+
+  const minutes = differenceInMinutes(currDate, hourDate);
+  const minuteDate = add(hourDate, {
+    minutes,
+  });
+  if (minutes !== 0) arr.push(`${minutes} minutes`);
+
+  const seconds = differenceInSeconds(currDate, minuteDate);
+  if (seconds !== 0) arr.push(`${seconds} seconds`);
+  return arr.join(", ");
+};
+
+const HeaderDate = ({ date }) => {
+  const [text, setText] = useState("");
+
+  useInterval(() => {
+    setText(dateToText(date));
+  }, 1000);
+
+  return <span>{text} ago</span>;
+};
+
+export default function Home({ json }) {
+  const parsed = useMemo(
+    () => json.map((item) => ({ ...item, date: parseDate(item.date) })),
+    []
+  );
+
+  return (
+    <div className="w-full h-screen flex justify-center flex-col items-center text-center">
+      <h1 className="text-xl font-semibold text-gray-900">
+        When did my car last turn off while I was driving it?
+      </h1>
+      <h2 className="text-4xl font-bold mt-6">
+        <HeaderDate date={parsed[0].date} />
+      </h2>
+      <h3 className="mt-3 text-xl text-gray-800">
+        on {format(parsed[0].date, "MMMM co, yyyy 'at' hh:mm a")}
+      </h3>
+      <p className="max-w-xs mt-8 text-gray-500">{parsed[0].comment}</p>
+    </div>
+  );
+}
+
+const parseDate = (date) =>
+  zonedTimeToUtc(
+    parse(date, "MM-dd-yyyy hh:mm a", new Date()),
+    "America/Chicago"
+  );
+
+export const getStaticProps = async () => {
+  const filePath = path.join(
+    path.resolve(process.cwd(), "pages"),
+    "../events.json"
+  );
+  const file = await fs.readFile(filePath);
+  const json = Array.from(JSON.parse(file)).sort(
+    (a, b) => parseDate(a.date) - parseDate(b.date)
+  );
+
+  return {
+    props: {
+      json,
+    },
+  };
+};
